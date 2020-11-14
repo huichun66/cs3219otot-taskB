98250ddc37f3749f67ccb405d1b18235b6bc6191
huichun66 <pewpewt97@gmail.com>
2020-11-14 14:29:47 +0800

    Test travis

--- resthub/test/contact.js
+++ resthub/test/contact.js
@@ -15,6 +15,8 @@ chai.use(chaiHttp);
 //Our parent block
 describe('Contacts', () => {
     beforeEach((done) => { //Before each test we empty the database
+        setTimeout(done, 10000);
+
         Contact.deleteMany({}, (err) => {
            done();
         });
